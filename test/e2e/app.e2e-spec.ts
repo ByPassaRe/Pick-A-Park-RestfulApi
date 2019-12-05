b8fc003fcf20462d4185c532d26f1e48961b755a
Michele Biondi <michelebiondi01@gmail.com>
2019-12-05 10:04:06 +0100

    Fixed e2e tests + moved to their own folder

--- test/e2e/app.e2e-spec.ts
+++ test/e2e/app.e2e-spec.ts
@@ -1,10 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from '../src/app/app.module';
+import { AppModule } from './../../src/app/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+
+  afterAll(() => app.close());
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -15,10 +17,11 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/ (GET)', (done) => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Hello World!')
+      .end(() => done());
   });
 });
