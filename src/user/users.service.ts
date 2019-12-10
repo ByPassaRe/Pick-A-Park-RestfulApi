c05358102ce15886e244aa27091ef67d9c81ded4
Michele Biondi <michelebiondi01@gmail.com>
2019-12-10 22:53:43 +0100

    Converted user to manual crud

--- src/user/users.service.ts
+++ src/user/users.service.ts
@@ -1,11 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { User } from './user.entity';
+import { Repository, DeleteResult, UpdateResult } from 'typeorm';
 
 @Injectable()
-export class UsersService extends TypeOrmCrudService<User> {
-  constructor(@InjectRepository(User) repo) {
-    super(repo);
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+
+  async findOne(id: number): Promise<User> {
+    return await this.userRepository.findOne(id);
+  }
+
+  async create(user: User): Promise<User> {
+    return await this.userRepository.save(user);
+  }
+
+  async update(user: User): Promise<UpdateResult> {
+    return await this.userRepository.update(user.id, user);
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    return await this.userRepository.delete(id);
   }
 }
