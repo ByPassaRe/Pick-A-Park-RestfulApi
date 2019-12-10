ecfb20b21cd75f40b5b4d51798f2083ce8f56f3c
Michele Biondi <michelebiondi01@gmail.com>
2019-12-10 23:43:21 +0100

    Add api docs with Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as helmet from 'helmet';
 
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Pick a Park API')
+    .setDescription('The Pick a Park API ')
+    .setVersion('1.0')
+    .addTag('parking')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   app.use(helmet());
+
   await app.listen(3000);
 }
 bootstrap();
