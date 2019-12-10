681b9a84fe250848b73f3c4221d06b0fc67ef290
Michele Biondi <michelebiondi01@gmail.com>
2019-12-10 12:35:29 +0100

    Add helmet js security features

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import * as helmet from 'helmet';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   await app.listen(3000);
 }
 bootstrap();
