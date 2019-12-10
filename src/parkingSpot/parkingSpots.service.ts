bda3f69e48a235618d24b0d99736be5a2fa9e29a
Michele Biondi <michelebiondi01@gmail.com>
2019-12-10 17:43:49 +0100

    Implemented distance

--- src/parkingSpot/parkingSpots.service.ts
+++ src/parkingSpot/parkingSpots.service.ts
@@ -3,9 +3,36 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { ParkingSpot } from './parkingSpot.entity';
 
+import { getDistance } from 'geolib';
+import { GeolibInputCoordinates } from 'geolib/es/types';
+
 @Injectable()
 export class ParkingSpotsService extends TypeOrmCrudService<ParkingSpot> {
   constructor(@InjectRepository(ParkingSpot) repo) {
     super(repo);
+  }
+
+  async retrieveNearestParkingSpot() {
+
+    const currentPosition: GeolibInputCoordinates = {
+      latitude: 0.0,
+      longitude: 0.0,
+    };
+
+    const parkingSpots = await this.repo.find();
+
+    const nearestParkingSpot = parkingSpots.map( parkingSpot => {
+
+      const parkingSpotLocation: GeolibInputCoordinates = {
+        latitude: parkingSpot.latitude,
+        longitude: parkingSpot.longitude,
+      };
+
+      const distance = getDistance(currentPosition, parkingSpotLocation);
+
+      return distance;
+    }).reduce((minDistance, currentDistance) => minDistance < currentDistance ? minDistance : currentDistance);
+
+    return nearestParkingSpot;
   }
 }
